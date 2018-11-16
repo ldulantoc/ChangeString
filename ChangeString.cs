3682935ed8c547b8380f62e1439e439731b0ac21
ldulantoc <45088461+ldulantoc@users.noreply.github.com>
2018-11-16 00:06:35 -0500

    Add files via upload

---
+++ ChangeString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class ChangeString
+{
+    static Char[] abecedario = "abcdefghijklmnñopqrstuvwxyz".ToCharArray();
+
+    static void Main(string[] args)
+    {
+        Console.Write("Entrada: ");
+        String cadena = Console.ReadLine();
+        Console.WriteLine("Salida: " + Build(cadena));
+    }
+
+    static string Build(string cadena)
+    {
+        String resultado = "";
+        int posicion;
+        Char caracter;
+
+        for (int i = 0; i < cadena.Length; i++)
+        {
+            caracter = cadena[i];
+            if (abecedario.Contains(caracter))
+            {
+                posicion = Array.IndexOf(abecedario, caracter);
+                if (posicion == abecedario.Length - 1)
+                    caracter = abecedario[0];
+                else
+                    caracter = abecedario[posicion + 1];
+            }
+            resultado += caracter;
+        }
+        return resultado;
+    }
+}
